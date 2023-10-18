0f811736379d52dcf9d9ee8c09a26b809bf5f8c8
Murylo Capucho <mcapuchoribeiro@gmail.com>
2023-10-18 15:20:59 -0300

    Implementação da Infra ECR

--- infra/terraform/aws-ecr.tf
+++ infra/terraform/aws-ecr.tf
@@ -1,5 +1,10 @@
 resource "aws_ecr_repository" "mixfast_ecr_repository" {
-  name = "${var.name}-ecr"
+  name = var.name
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 
   tags = var.tags
 }
