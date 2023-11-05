0de6ded1abca734b888443e9e2646a3e8c17e088
Murylo Capucho <mcapuchoribeiro@gmail.com>
2023-11-05 16:55:35 -0300

    Implementação da Infra ECR

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -8,3 +8,29 @@ resource "aws_ecr_repository" "mixfast_ecr_repository" {
 
   tags = var.tags
 }
+
+resource "aws_ecr_repository_policy" "this" {
+  repository = aws_ecr_repository.mixfast_ecr_repository.name
+  policy     = <<EOF
+ {
+   "Version": "2008-10-17",
+   "Statement": [
+     {
+       "Sid": "policy ecr access repository",
+       "Effect": "Allow",
+       "Principal": "*",
+       "Action": [
+         "ecr:BatchCheckLayerAvailability",
+         "ecr:BatchGetImage",
+         "ecr:CompleteLayerUpload",
+         "ecr:GetDownloadUrlForLayer",
+         "ecr:GetLifecyclePolicy",
+         "ecr:InitiateLayerUpload",
+         "ecr:PutImage",
+         "ecr:UploadLayerPart"
+       ]
+     }
+   ]
+ }
+ EOF
+}
