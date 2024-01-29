08aa66f199c8d7bb4ab8dce91f28e28ba7231787
Murylo Capucho <mcapuchoribeiro@gmail.com>
2024-01-28 23:16:17 -0300

    Implementação da Infra ECR

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -20,6 +20,17 @@ resource "aws_ecr_repository" "mixfastpagamento_ecr_repository" {
   tags = var.tags
 }
 
+resource "aws_ecr_repository" "mixfastproducao_ecr_repository" {
+  name = "${var.name}producao"
+  image_tag_mutability = var.image_tag_mutability
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = var.tags
+}
+
 resource "aws_ecr_repository_policy" "this" {
   repository = aws_ecr_repository.mixfast_ecr_repository.name
   policy     = <<EOF
