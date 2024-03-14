9cc612690f7be5c7fbd9cb089d7496d8a866b42b
Murylo Capucho <mcapuchoribeiro@gmail.com>
2024-03-13 21:27:14 -0300

    Implementação da Infra ECR

--- infra/terraform/versions.tf
+++ infra/terraform/versions.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "mixfast-s3-bucket-ecr"
+    bucket = "fiap-mixfast-s3-bucket-ecr"
     key    = "mixfast-infrastructure"
     region = "us-east-1"
   }
